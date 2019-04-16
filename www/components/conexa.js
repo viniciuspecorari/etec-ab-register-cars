cf8999f187ab1729b055e82ecb652a953c5ec706
vprzo12 <odantewz@gmail.com>
2019-04-17 05:47:06 +0900

    sript do banco concluida e comunicação em andamento

--- www/components/conexa.js
+++ www/components/conexa.js
@@ -1,7 +1,20 @@
 // This is a JavaScript file
 
-//criando as variaveis
+
 
 $(document).on('click', '#btncadastrar', function(){
+  //pegando os parametros
+  var parametros = {
+      "marca":$('#marca').val,
+      "modelo":$('#modelo').val,
+      "ano":$('#ano').val,
+      "cor":$('#cor').val,
+      "valor":$('#valor').val,
+  }
+
+  $.ajax({
+    type: "post",
+    url: "https://cadastro-carros-3i2-onoilvp.c9users.io/cadastroCarro3i2.php"    
+  })
 
 });
